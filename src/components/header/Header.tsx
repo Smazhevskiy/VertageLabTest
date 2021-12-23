c5ccc28534bfe8ea3e4bbc0c570afd2e438a2ad0
user_nitro_5 <timmy.lepel@gmail.com>
2021-12-23 20:16:33 +0300

    refactoring home/addNew components

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -1,43 +1,14 @@
-import React, {ChangeEvent, useState} from 'react'
-import {Input} from '../input/Input'
+import React from 'react'
 import s from './Header.module.css'
 import {Button} from '../button/Button'
-import {useDispatch} from 'react-redux'
-import {v4} from 'uuid'
-import {addNewGradient} from '../../store/gradient-reducer'
 
 
 export const Header = () => {
-    const dispatch = useDispatch()
-    let [color1, setColor1] = useState<string>('')
-    let [color2, setColor2] = useState<string>('')
 
-
-    const onChangeInput1Handler = (e: ChangeEvent<HTMLInputElement>) => setColor1(e.currentTarget.value)
-    const onChangeInput2Handler = (e: ChangeEvent<HTMLInputElement>) => setColor2(e.currentTarget.value)
-    const clickButtonHandler = () => {
-        dispatch(addNewGradient({id: v4(), color1, color2}))
-    }
 
     return (
         <div className={s.header}>
-            <div className={s.inputsBlock}>
-                <div className={s.inputBody}>
-                    <Input
-                        value={color1}
-                        placeholder={'введите hex-code цвета'}
-                        onChange={onChangeInput1Handler}
-                    />
-                </div>
-                <div className={s.inputBody}>
-                    <Input
-                        value={color2}
-                        placeholder={'введите hex-code цвета'}
-                        onChange={onChangeInput2Handler}
-                    />
-                </div>
-            </div>
-            <Button onClick={clickButtonHandler}>Создать</Button>
+            <Button >add new color</Button>
         </div>
     )
 }
--- src/pages/new/AddNewGradient.tsx
+++ src/pages/new/AddNewGradient.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
+import React, {ChangeEvent, useState} from 'react'
 import s from './AddNewGradient.module.css'
+import {Input} from '../../components/input/Input'
+import {Button} from '../../components/button/Button'
+import {useDispatch} from 'react-redux'
+import {addNewGradient} from '../../store/gradient-reducer'
+import {v4} from 'uuid'
 
 export const AddNewGradient = () => {
+    const dispatch = useDispatch()
+    let [color1, setColor1] = useState<string>('')
+    let [color2, setColor2] = useState<string>('')
+
+
+    const onChangeInput1Handler = (e: ChangeEvent<HTMLInputElement>) => setColor1(e.currentTarget.value)
+    const onChangeInput2Handler = (e: ChangeEvent<HTMLInputElement>) => setColor2(e.currentTarget.value)
+    const clickButtonHandler = () => {
+        dispatch(addNewGradient({id: v4(), color1, color2}))
+    }
+
+
     return (
         <div className={s.addNew}>
-            AddNewGradient
+            <div className={s.inputsBlock}>
+                <div className={s.inputBody}>
+                    <Input
+                        value={color1}
+                        placeholder={'введите hex-code цвета'}
+                        onChange={onChangeInput1Handler}
+                    />
+                </div>
+                <div className={s.inputBody}>
+                    <Input
+                        value={color2}
+                        placeholder={'введите hex-code цвета'}
+                        onChange={onChangeInput2Handler}
+                    />
+                </div>
+            </div>
+            <Button onClick={clickButtonHandler}>Создать</Button>
         </div>
     )
 }
